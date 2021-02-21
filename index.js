375d17bcda44998ed83ee84e4953e33c07a18ea8
Shaviaditya <rudrashiv311@gmail.com>
2021-02-21 20:28:12 +0530

    changes

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ app.get('/',(req,res) =>{
         user = 'user';
         key = 'lWITX3UWWjh505HkmkptIQ==';
         res.cookie(user,key,{httpOnly: true});
-        res.sendFile(path.join(__dirname,'public','index.html'));
+        res.sendFile(path.join(__dirname,'index.html'));
     }
 
 });
 app.get('/robots.txt',(req,res) =>{
     res.sendFile(path.join(__dirname,'public','robots.txt'));
 });
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join()));
 var PORT = process.env.PORT || 8080;
 app.listen(PORT,() => console.log(`Server started at ${PORT}`));
