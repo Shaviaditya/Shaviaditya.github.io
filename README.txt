530c297bc1a24ab25fd97ec64f80aea235b04025
Shaviaditya <rudrashiv311@gmail.com>
2021-02-21 19:29:48 +0530

    first commit

---
+++ README.txt
@@ -0,0 +1,15 @@
+This is a task that I was assigned by GLUG NITDGP on working with cookies to create a CTF Question based on it.
+You can visit the site using the URL:
+https://shaviaditya.github.io/
+I have made the project using VSCode Editor and using liveserver to host it to work with it.
+It has the following files:
+index.html
+main.js
+sol.html
+Link.txt
+style.css
+The index.html is the parent file that controls all the other files to launch the Question on a local host using liveserver.
+Have fun working on solving the question.
+Since I'm a beginner and is my first attempt at making even a Level1 Question on CTF please consider my efforts.
+Thanks, Rudraditya
+May the source be with you :)
---
+++ index.js
@@ -0,0 +1,26 @@
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const  app = express();
+app.use(cookieParser());
+var user='';
+var key = '';
+app.get('/',(req,res) =>{
+    if (req.cookies.user == 'hONBMRfJJl1YyLptN8oRzg=='){
+        res.sendFile(path.join(__dirname,'public','sol.html'));
+    }
+    else{
+        user = 'user';
+        key = 'lWITX3UWWjh505HkmkptIQ==';
+        res.cookie(user,key,{httpOnly: true});
+        res.sendFile(path.join(__dirname,'public','index.html'));
+    }
+
+});
+app.get('/robots.txt',(req,res) =>{
+    res.sendFile(path.join(__dirname,'public','robots.txt'));
+});
+app.use(express.static(path.join(__dirname,'public')));
+var PORT = process.env.PORT || 8080;
+app.listen(PORT,() => console.log(`Server started at ${PORT}`));
